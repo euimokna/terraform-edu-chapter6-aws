1adeb9bef421961280672c7ac453b4421e8101b6
euimokna <euimok@gmail.com>
2022-07-29 14:21:56 +0900

    bb

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ provider "aws" {
   default_tags {
     tags = {
       #Project = "Coffee-Mug-Cake"
-      Project = "tom defualt tag2"      
+      Project = "tom defualt tag3"      
     }
   }
 }
